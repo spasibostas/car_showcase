98a1f05846e4375d834ee3e1a9bd11323717fff8
StasDemidovich <demidovichstas98@gmail.com>
2023-09-04 18:46:56 +0300

    start of filter creation

---
+++ utils/index.ts
@@ -0,0 +1,17 @@
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '13e44e488emsha1217d2c3cfd214p17afdbjsnda82f77a82df',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+
