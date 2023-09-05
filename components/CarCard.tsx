fd8ee86769589675c27213c9d5e426a26b4ca546
StasDemidovich <demidovichstas98@gmail.com>
2023-09-05 15:46:41 +0300

    add car cards

---
+++ components/CarCard.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+import { CarProps } from "@/types";
+import { calculateCarRent } from "@/utils";
+import Image from 'next/image';
+import CustomButton from './CustomButton';
+import { useState } from 'react';
+import CarDetails from './CarDetails';
+
+interface CarCardProps {
+    car: CarProps;
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+
+  const { city_mpg, year, make, model, transmission, drive } = car;
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const carRent = calculateCarRent(city_mpg, year);
+
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        <span className="self-start font-semibold text-[14px]">$</span>
+        {carRent}
+        <span className="self-end font-medium text-[14px]">/day</span>
+      </p>
+
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image
+          src="/hero.png"
+          fill
+          priority
+          alt="car model"
+          className="object-contain"
+        />
+      </div>
+
+      <div className="relative flex w-full mt-2">
+        <div className="flex w-full group-hover:invisible justify-between text-grey">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src="/steering-wheel.svg"
+              alt="steering wheel"
+              width={20}
+              height={20}
+            />
+            <p className="text-[14px]">
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/tire.svg" alt="tire" width={20} height={20} />
+            <p className="text-[14px]">{drive.toUpperCase()}</p>
+          </div>
+
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/gas.svg" alt="gas" width={20} height={20} />
+            <p className="text-[14px]">{city_mpg} MPG</p>
+          </div>
+        </div>
+
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="View more"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
+      </div>
+
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
+    </div>
+  );
+}
+
+export default CarCard
--- utils/index.ts
+++ utils/index.ts
@@ -1,17 +1,27 @@
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-	method: 'GET',
-	headers: {
+export async function fetchCars() {
+	const headers = {
 		'X-RapidAPI-Key': '13e44e488emsha1217d2c3cfd214p17afdbjsnda82f77a82df',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
-};
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
+	const resnonse = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+		headers: headers,
+	});
+
+	const result = await resnonse.json();
+
+	return result;
 }
 
+export const calculateCarRent = (city_mpg: number, year: number) => {
+	const basePricePerDay = 50;
+	const mileageFactor = 0.1;
+	const ageFactor = 0.05;
+  
+	const mileageRate = city_mpg * mileageFactor;
+	const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  
+	const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  
+	return rentalRatePerDay.toFixed(0);
+  };
