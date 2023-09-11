a7e559c66c7b5bb833f28ba5cef2e950a73960d7
StasDemidovich <demidovichstas98@gmail.com>
2023-09-11 15:03:36 +0300

    add fetching a data by custom filters

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,19 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+
+	const { manufaturer, year, fuel, limit, model} = filters;
+
 	const headers = {
 		'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY || '',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
 
-	const resnonse = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
+	const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufaturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
 		headers: headers,
 	});
 
-	const result = await resnonse.json();
+	const result = await response.json();
 
 	return result;
 }
